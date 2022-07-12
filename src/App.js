2811d5f62e2bf532247dd29d89502871382a3176
Tristan <tsilvas0113@gmail.com>
2022-07-12 08:49:42 -0500

    Create function of an array of 10 random numbers

--- src/App.js
+++ src/App.js
@@ -2,6 +2,15 @@ import React from "react";
 import Die from "./components/Die";
 
 function App() {
+
+  function allNewDice() {
+    let randArray = []
+    for (let i = 0; i < 10; i++) {
+      randArray.push(Math.floor(Math.random() * 6) + 1)
+    }
+    return randArray
+  }
+
   return (
     <main>
       <Die 
